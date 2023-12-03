8598cbf4add0ab343b9bd80dfec7fe0a4c62b37d
dylancicone <113648749+dylancicone@users.noreply.github.com>
2023-12-03 11:37:45 -0500

    I can't figure out how to make it so that when I click on the body of the page it doesn't change yellow. I just want the divs to change yellow in the grid when I click them. I want to be able to use Java when I'm doing the actual algorithm to form new generations but I'm not sure how. I think I might have to use a Servlet? Still working.

--- click.js
+++ click.js
@@ -1,4 +1,17 @@
-$(document).click(function(event) {
-    var text = $(event.target).text();
-    console.log(text);
-});
+document.addEventListener('click', function(e) {
+  e = e || window.event;
+  target = e.target || e.srcElement,
+      text = target.textContent || target.innerText;   
+  if(target != document.getElementById("body") && target != document.getElementById("html")){
+    update(e, target);
+  }
+}, false);
+
+function update(e, targ){
+  const elem = e.target.innerText;
+  console.log(elem);
+  
+  targ.style.backgroundColor = "yellow";
+  targ.style.color = "yellow";
+  
+}
